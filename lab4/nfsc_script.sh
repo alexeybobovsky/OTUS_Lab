e7aef5acfcba3e7688508b8fd1440def2da74c37
Бобовский Алексей Николаевич <BobovskyAN@itc-c.local>
2020-09-01 18:34:56 +0800

    add vers&proto2fstab

--- lab4/nfsc_script.sh
+++ lab4/nfsc_script.sh
@@ -2,4 +2,4 @@ sudo yum install nfs-utils -y
 sudo systemctl start rpcbind && sudo systemctl enable rpcbind
 sudo mkdir /nfs
 sudo mount -t nfs -o vers=3 -o proto=udp  192.168.50.10:/shared /nfs
-echo -e "192.168.50.10:/shared  /nfs     nfs    defaults    0 0" | sudo tee --append /etc/fstab
+echo -e "192.168.50.10:/shared  /nfs     nfs    nfsvers=3,udp    0 0" | sudo tee --append /etc/fstab
